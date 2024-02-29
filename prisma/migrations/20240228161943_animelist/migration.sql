4d8be4c7350f0367973b611712d45fe3c3611b64
lutfi1506 <lutfirahman248@gmail.com>
2024-03-01 01:27:13 +0700

    memperbaiki database

--- prisma/migrations/20240228161943_animelist/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `user_image` on the `collection` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `collection` DROP COLUMN `user_image`;
---
+++ prisma/migrations/20240229182241_animelist/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Collection" (
+    "id" SERIAL NOT NULL,
+    "anime_mal_id" TEXT NOT NULL,
+    "user_email" TEXT NOT NULL,
+    "anime_image" TEXT,
+    "anime_title" TEXT,
+
+    CONSTRAINT "Collection_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Collection_user_email_anime_mal_id_key" ON "Collection"("user_email", "anime_mal_id");
